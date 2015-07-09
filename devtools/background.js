843384716a7e51f30cf26e4aedb8e6238d8d4163
Zack Argyle <@pinterest.com>
2015-07-09 11:03:56 -0700

    Initial commit

---
+++ devtools/background.js
@@ -0,0 +1,42 @@
+var panels = chrome.devtools.panels;
+
+/* 
+ * Keep this function flat because we call .toString() on it below. Cannot
+ * reference any other functions
+ */
+function getPanelContents() {
+    if (window.P && $0) {
+        var app = window.P.app;
+        var module, element;
+
+        // Find first module
+        while (!module) {
+            element = element ? element.parentNode : $0;
+            if (element === document.body) {
+                module = {};
+            } else {
+                module = app._getDescendantByElement(element);
+            }
+        }
+        // window.pinmod = module;
+
+        // Shallow copy contents
+        var contents = {};
+        Object.keys(module).forEach(function(key) {
+            contents[key] = module[key];
+        });
+        // Add a readable name from prototype className
+        contents.$$Module = module.className;
+        return contents;
+    } else {
+        return {};
+    }
+}
+
+panels.elements.createSidebarPane("Module", function (sidebar) {
+    panels.elements.onSelectionChanged.addListener(function updateElementProperties() {
+        sidebar.setExpression("(" + getPanelContents.toString() + ")()");
+    });
+});
+
+var pinspector = panels.create("Pinspector", "img/angular.png", "panel/app.html");
--- panel/app.js
+++ panel/app.js
@@ -1,16 +1,17 @@
 'use strict';
 
-angular.module('batarang.app', [
-  'batarang.app.hint',
-  'batarang.app.scopes',
+// angular.module('batarang.app', [
+//   'batarang.app.hint',
+//   'batarang.app.scopes',
 
-  'batarang.scope-tree',
-  'batarang.code',
-  'batarang.inspected-app',
-  'batarang.json-tree',
-  'batarang.scope-tree',
-  'batarang.tabs',
-  'batarang.vertical-split'
-]).
-// immediately instantiate this service
-run(['inspectedApp', angular.noop]);
+//   'batarang.scope-tree',
+//   'batarang.code',
+//   'batarang.inspected-app',
+//   'batarang.json-tree',
+//   'batarang.scope-tree',
+//   'batarang.tabs',
+//   'batarang.vertical-split'
+// ]).
+// // immediately instantiate this service
+// run(['inspectedApp', angular.noop]);
+console.log(window);
