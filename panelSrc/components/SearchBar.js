7cae67abda68af1bfb9fcba6708d94e073efaf53
James Wu <jwu@efunda.com>
2015-07-13 17:14:18 -0700

    addressed @zackargyle comments

--- panelSrc/components/SearchBar.js
+++ panelSrc/components/SearchBar.js
@@ -1,33 +1,49 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-  onKeyUp() {
-    if (this.props.root.length <= 0) {
-        return;
+    debounce(func, wait) {
+        var timeout;
+        return function() {
+            var context = this, args = arguments;
+            var later = function () {
+                timeout = null;
+            };
+            var call = !timeout;
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+            if (call) func.apply(context, args);
+        };
     }
 
-    var query = new RegExp("(" + document.getElementById('search-input').value + ")", "gim");
+    onKeyUp(evt) {
+        if (this.props.root.length <= 0) {
+            return;
+        }
 
-    for (var i = 0; i < this.props.root.length; i++) {
-        var el = this.props.root[i];
-        var text = el.value;
-        var e = el.innerHTML;
-        var en = e.replace(/(<span>|<\/span>)/igm, "");
-        this.props.root[i].innerHTML = en;
-        var ne = en.replace(query, "<span>$1</span>");
-        this.props.root[i].innerHTML = ne;
-    };
-  }
+        var query = new RegExp("(" + evt.target.value + ")", "gim");
 
-  render() {
-      return (
-          <div id="toolbar">
-              <span id="search-control">
-                  <input id="search-input" placeholder="Find" onKeyUp={this.onKeyUp.bind(this)} />
-              </span>
-          </div>
-      );
-  }
+        for (var i = 0; i < this.props.root.length; i++) {
+            var el = this.props.root[i];
+            var e = el.innerHTML;
+            var en = e.replace(/(<span>|<\/span>)/igm, "");
+            var ne = en.replace(query, "<span>$1</span>");
+            this.props.root[i].innerHTML = ne;
+        };
+    }
+
+    render() {
+        return (
+            <div id="toolbar">
+                <span id="search-control">
+                    <input
+                        id="search-input"
+                        placeholder="Find"
+                        onKeyUp={this.debounce(this.onKeyUp.bind(this), 100).bind(this)}
+                    />
+                </span>
+            </div>
+        );
+    }
 }
 SearchBar.propTypes = {
     root: React.PropTypes.object
